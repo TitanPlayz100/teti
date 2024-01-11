3f9de33e3741a116a24b96e7b999e983b90add6a
TitanPlayz <118145727+TitanPlayz100@users.noreply.github.com>
2024-01-11 11:59:43 +1100

    added hold and das

--- code.js
+++ code.js
@@ -1,33 +1,77 @@
 // GLOBALS
 let loopfunction = null;
-let rotationState = null;
+let rotationState = 1;
 let currentPiece = null;
+let heldpiece = { piece: null, occured: false };
 let currentLoc = null;
+let gameEnd = false;
+let remainingpieces = [[], []]
+let dasfunc = null;
+let arrfunc = null;
+let sdarrfunc = null;
+
+// settings
+const grid = true;
 const arr = 50;
 const das = 150;
-const gravitySpeed = 1000
+const sdarr = 100;
+const gravitySpeed = 1000;
 
-const rightKey = 'ArrowRight'
-const leftKey = 'ArrowLeft'
-const cwKey = 'ArrowUp'
-const ccwKey = 'z'
-const hdKey = ' '
+const rightKey = 'ArrowRight';
+const leftKey = 'ArrowLeft';
+const cwKey = 'ArrowUp';
+const ccwKey = 'z';
+const hdKey = ' ';
+const sdKey = 'ArrowDown';
+const hold = 'c';
 
 function main() {
-    drawPiece(randomiser())
-    loopfunction = setInterval(() => {movePieceDown(false)}, gravitySpeed);
+    if (grid) drawGrid();
+    drawPiece(randomiser());
+    loopfunction = setInterval(() => { movePieceDown(false) }, gravitySpeed);
 }
 
-// Key listeners
+// Keyboard listeners
 this.addEventListener('keydown', event => {
     if (event.repeat) return;
+    if (gameEnd) return;
     if (event.key == rightKey) movePiece("RIGHT");
     if (event.key == leftKey) movePiece("LEFT");
     if (event.key == cwKey) rotate("CW");
     if (event.key == ccwKey) rotate("CCW");
     if (event.key == hdKey) movePieceDown(true);
+    if (event.key == hold) holdPiece();
+
+    // set das times
+    if (event.key == rightKey || event.key == leftKey) {
+        dasfunc = setTimeout(() => {
+            arrfunc = setInterval(() => {
+                if (event.key == rightKey) movePiece("RIGHT");
+                if (event.key == leftKey) movePiece("LEFT");
+            }, arr);
+        }, das);
+    }
+
+    if (event.key == sdKey) {
+        movePieceDown(false);
+        sdarrfunc = setInterval(() => {
+            if (event.key == sdKey) movePieceDown(false);
+        }, sdarr);
+    }
 });
 
+this.addEventListener('keyup', event => { if (event.key == rightKey || event.key == leftKey || event.key == sdKey) endDasArr() });
+
+function endDasArr() {
+    clearTimeout(dasfunc);
+    clearInterval(arrfunc);
+    clearInterval(sdarrfunc)
+    dasfunc = null;
+    arrfunc = null;
+    sdarrfunc = null;
+}
+
+// piece movements
 function rotate(type) {
     if (currentPiece.name == 'o') { return; }
     let stoprotate = false;
@@ -37,7 +81,7 @@ function rotate(type) {
     const DOMboard = document.getElementsByClassName('board')[0];
     const currentShape = currentPiece['shape' + newrotationstate];
     for (let column = 0; column < currentShape.length; column++) {
-        for (let row = 0; row < currentShape.length; row++) { // THERE MAY BE A PROBLEM WITH currentShape.length being the same
+        for (let row = 0; row < currentShape.length; row++) {
             if (currentShape[column][row] == 1) {
                 let mino = document.createElement('div');
                 let newcol = column + currentLoc[1] + 1
@@ -104,32 +148,20 @@ function movePieceDown(instant) {
     const DOMminostopped = document.querySelectorAll('.stopped');
     DOMminos.forEach((piece) => {
         let gridarea = piece.style.gridArea.split('/');
-        if (gridarea[0] > 19) {
+        if (gridarea[0] > 19) { // reached bottom of board
             stopfalling = true;
-            DOMminos.forEach((mino) => {
-                mino.classList.remove('active');
-                mino.classList.add('stopped')
-            })
+            DOMminos.forEach((mino) => { mino.classList.remove('active'); mino.classList.add('stopped'); });
         }
         DOMminostopped.forEach((stoppedmino) => {
             let gridarea2 = stoppedmino.style.gridArea.split('/');
             if ((Number(gridarea[0]) + 1) == Number(gridarea2[0]) && Number(gridarea[1]) == Number(gridarea2[1])) {
                 if (Number(gridarea[0]) == 2) endGame();
                 stopfalling = true;
-                DOMminos.forEach((mino) => {
-                    mino.classList.remove('active');
-                    mino.classList.add('stopped');
-                });
+                DOMminos.forEach((mino) => { mino.classList.remove('active'); mino.classList.add('stopped'); });
             }
         });
     });
-    if (stopfalling == true) { // Has hit ground
-        rotationState = 1;
-        clearLines()
-        drawPiece(randomiser())
-        stoppiece = false;
-        return;
-    }
+    if (stopfalling == true) { hitGround(); return; }
     DOMminos.forEach((mino) => { // has not hit ground
         let gridarea = mino.style.gridArea.split('/');
         let newheight = Number(gridarea[0]) + 1;
@@ -140,7 +172,17 @@ function movePieceDown(instant) {
     if (instant) movePieceDown(true);
 }
 
+// gameplay mechanics
+function hitGround() {
+    rotationState = 1;
+    heldpiece.occured = false;
+    endDasArr()
+    clearLines()
+    drawPiece(randomiser())
+}
+
 function endGame() {
+    gameEnd = true;
     clearInterval(loopfunction);
     console.log('END');
 }
@@ -154,80 +196,56 @@ function clearLines() {
     });
 
     for (const key in columns) {
-        if (columns[key] >= 10) {
-            DOMminostopped.forEach((stoppedmino) => {
-                let row = Number(stoppedmino.style.gridArea.split('/')[0]);
-                if (row == key) {
-                    stoppedmino.remove();
-                }
-            });
-            DOMminostopped.forEach((stoppedmino) => {
-                let row = Number(stoppedmino.style.gridArea.split('/')[0]);
-                if (row < key) {
-                    let gridarea = stoppedmino.style.gridArea.split('/');
-                    let newheight = Number(gridarea[0]) + 1;
-                    gridarea.splice(0, 1);
-                    stoppedmino.style.gridArea = newheight + "/" + gridarea.join('/');
-                }
-            });
-        }
+        if (columns[key] < 10) continue;
+        DOMminostopped.forEach((stoppedmino) => { // clear minos in line
+            if (Number(stoppedmino.style.gridArea.split('/')[0]) != key) return;
+            stoppedmino.remove();
+        });
+        DOMminostopped.forEach((stoppedmino) => { // move minos down
+            let row = Number(stoppedmino.style.gridArea.split('/')[0]);
+            if (row >= key) return;
+            let gridarea = stoppedmino.style.gridArea.split('/');
+            let newheight = Number(gridarea[0]) + 1;
+            gridarea.splice(0, 1);
+            stoppedmino.style.gridArea = newheight + "/" + gridarea.join('/');
+        });
     }
 }
 
-const remainingpieces = [[], []]
 function randomiser() {
     if (remainingpieces[1].length == 0) {
         pieces.forEach((piece) => remainingpieces[1].push(piece.name))
-        let unshuffled = remainingpieces[1];
-        let shuffled = unshuffled
-            .map(value => ({ value, sort: Math.random() }))
-            .sort((a, b) => a.sort - b.sort)
-            .map(({ value }) => value)
-        remainingpieces[1] = shuffled
+        remainingpieces[1] = shuffleArray(remainingpieces[1])
     }
     if (remainingpieces[0].length == 0) {
         remainingpieces[0] = remainingpieces[1];
         remainingpieces[1] = [];
         pieces.forEach((piece) => remainingpieces[1].push(piece.name))
-        let unshuffled = remainingpieces[1];
-        let shuffled = unshuffled
-            .map(value => ({ value, sort: Math.random() }))
-            .sort((a, b) => a.sort - b.sort)
-            .map(({ value }) => value)
-        remainingpieces[1] = shuffled
+        remainingpieces[1] = shuffleArray(remainingpieces[1])
     }
     const piece = remainingpieces[0][0];
     remainingpieces[0].splice(0, 1);
-    let index = 0;
-    for (let i = 0; i < pieces.length; i++) {
-        if (pieces[i].name == piece) {
-            index = i
-        }
-    }
-    return pieces[index]
+    return piece, pieces.filter((element) => { return element.name == piece })[0];
+}
+
+function shuffleArray(array) {
+    let unshuffled = array;
+    return unshuffled.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value);
 }
 
 function drawPiece(piece) {
     currentPiece = piece;
     const DOMboard = document.getElementsByClassName('board')[0];
-
-    const pieceheight = piece.shape1.length;
-    const piecelength = piece.shape1[0].length;
-    for (let column = 0; column < pieceheight; column++) {
-        for (let row = 0; row < piecelength; row++) {
-            if (piece.shape1[column][row] == 1) {
-                let mino = document.createElement('div');
-                if (piece.name == "i") {
-                    mino.style.gridArea = (column + 1) + "/" + (row + 1 + 3) + "/span 1/span 1"
-                    currentLoc = [3, 1];
-                } else {
-                    mino.style.gridArea = (column + 1) + "/" + (row + 1 + 4) + "/span 1/span 1"
-                    currentLoc = [4, 1];
-                }
-                mino.style.backgroundColor = piece.colour;
-                mino.classList.add('active')
-                DOMboard.appendChild(mino);
-            }
+    for (let column = 0; column < piece.shape1.length; column++) {
+        for (let row = 0; row < piece.shape1.length; row++) {
+            if (piece.shape1[column][row] != 1) continue; // check pattern specified to colour in
+            let mino = document.createElement('div');
+            let offset = (piece.name == 'o') ? 4 : 3;
+            mino.style.gridArea = (column + 1) + "/" + (row + 1 + offset) + "/span 1/span 1"
+            mino.style.backgroundColor = piece.colour;
+            mino.classList.add('active')
+            DOMboard.appendChild(mino);
+            currentLoc = [offset, 1];
         }
     }
     updateNext();
@@ -238,28 +256,63 @@ function updateNext() {
     const DOMNext = document.getElementsByClassName('next')[0];
     DOMNext.replaceChildren()
     for (let i = 0; i < first5.length; i++) {
-        let index = 0;
-        for (let j = 0; j < pieces.length; j++) {
-            if (pieces[j].name == first5[i]) {
-                index = j
-            }
-        }
-        const piece = pieces[index]
+        const piece = pieces.filter((element) => { return element.name == first5[i] })[0];
         const pieceheight = piece.shape1.length;
         const piecelength = piece.shape1[0].length;
         for (let column = 0; column < pieceheight; column++) {
             for (let row = 0; row < piecelength; row++) {
-                if (piece.shape1[column][row] == 1) {
-                    let mino = document.createElement('div');
-                    if (piece.name == "o") {
-                        mino.style.gridArea = (column + (3 * i) + 1) + "/" + (row + 2) + "/span 1/span 1"
-                    } else {
-                        mino.style.gridArea = (column + (3 * i) + 1) + "/" + (row + 1) + "/span 1/span 1"
-                    }
-                    mino.style.backgroundColor = piece.colour;
-                    DOMNext.appendChild(mino);
-                }
+                if (piece.shape1[column][row] != 1) continue; // pattern to colour in
+                let mino = document.createElement('div');
+                let offset = (piece.name == 'o') ? 2 : 1;
+                mino.style.gridArea = (column + (3 * i) + 1) + "/" + (row + offset) + "/span 1/span 1"
+                mino.style.backgroundColor = piece.colour;
+                DOMNext.appendChild(mino);
             }
+        }
+    }
+}
+
+function holdPiece() {
+    if (heldpiece.occured == true) return;
+    const DOMminos = document.querySelectorAll('.active');
+    DOMminos.forEach((mino) => mino.remove());
+
+    if (heldpiece.piece == null) {
+        heldpiece.piece = currentPiece;
+        hitGround();
+    } else {
+        let newpiece = heldpiece.piece;
+        heldpiece.piece = currentPiece;
+        currentPiece = newpiece;
+        rotationState = 1;
+        drawPiece(currentPiece);
+    }
+
+    const DOMheldpiece = document.querySelectorAll('.hold')[0];
+    DOMheldpiece.replaceChildren();
+    const pieceheight = heldpiece.piece.shape1.length;
+    const piecelength = heldpiece.piece.shape1[0].length;
+    for (let column = 0; column < pieceheight; column++) {
+        for (let row = 0; row < piecelength; row++) {
+            if (heldpiece.piece.shape1[column][row] != 1) continue; // pattern to colour in
+            let mino = document.createElement('div');
+            let offset = (heldpiece.piece.name == 'o') ? 2 : 1;
+            mino.style.gridArea = (column + 1) + "/" + (row + offset) + "/span 1/span 1"
+            mino.style.backgroundColor = heldpiece.piece.colour;
+            DOMheldpiece.appendChild(mino);
+        }
+    }
+    heldpiece.occured = true;
+}
+
+function drawGrid() {
+    const DOMboard = document.getElementsByClassName('board')[0];
+    for (let col = 0; col < 20; col++) {
+        for (let row = 0; row < 10; row++) {
+            let mino = document.createElement('div');
+            mino.style.gridArea = (col + 1) + "/" + (row + 1) + "/span 1/span 1"
+            mino.classList.add('gridLine');
+            DOMboard.appendChild(mino);
         }
     }
 }
@@ -311,7 +364,7 @@ const pieces = [
             [1, 1, 0],
             [0, 1, 0]
         ],
-        colour: "green"
+        colour: "LawnGreen"
     },
     {
         name: "o",
@@ -343,7 +396,7 @@ const pieces = [
             [1, 1, 0],
             [0, 1, 0]
         ],
-        colour: "purple"
+        colour: "DarkMagenta"
     },
     {
         name: "j",
@@ -391,7 +444,7 @@ const pieces = [
             [0, 1, 0],
             [0, 1, 0]
         ],
-        colour: "orange"
+        colour: "darkorange"
     },
     {
         name: "i",
@@ -421,6 +474,5 @@ const pieces = [
         ],
         colour: "aqua"
     }
-]
-
+];
 main();
