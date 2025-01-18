a40bf7bc4b7714797cf941371586a97912ff2286
TitanPlayz <118145727+TitanPlayz100@users.noreply.github.com>
2025-01-18 14:48:56 +1100

    small fix real

--- src/display/pixirender.js
+++ src/display/pixirender.js
@@ -457,6 +457,7 @@ export class PixiRender {
                 }
                 s.container.y = window.innerHeight - 40 + 4 * o * (t % 2 == 0 ? 1 : -1)
             let oldPB = localStorage.stats ? JSON.parse(localStorage.stats).pbs.zenith : undefined
+            if (oldPB.pbstats.floorTime == undefined) oldPB = undefined;
             if (floor === t + 1) {
                 let progress = (Game.stats.altitude - Game.zenith.FloorDistance[floor - 1]) / (Game.zenith.FloorDistance[floor]- Game.zenith.FloorDistance[floor - 1])
                 s.bottomText.style.fill = 16777215
